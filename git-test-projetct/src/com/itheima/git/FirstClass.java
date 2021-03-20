54c9a834774fd620e99c5d4b1d99eefbc088d7b0
caijinmei <caijinmei@iottepa.com>
2021-03-20 21:47:13 +0800

    把本地工程添加到git

---
+++ git-test-projetct/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+import java.sql.SQLOutput;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
